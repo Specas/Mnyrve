8611e1b9b8708eb4f2ee65e1dfbbab84e0c7026a
specas <shrenik95@gmail.com>
2018-10-25 16:25:17 -0400

    Utility functions to deal with non-technical parts of the project.
    - Functions to read paths from the tools/data files and return them.

---
+++ utility/include/path_util.h
@@ -0,0 +1,24 @@
+#ifndef PATH_UTIL_H
+#define PATH_UTIL_H
+
+
+#include "Python.h"
+#include "unistd.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace mnyrve {
+namespace utility {
+
+
+std::string GetCurrentDirectory();
+
+
+} // namespace mnyrve 
+} // namespace utility
+
+
+
+#endif
