118578b10b95a48da492ee4422909fe0cfd26ed4
shrenikm <shrenik95@gmail.com>
2018-11-04 02:57:57 -0500

    Finite mdp initial formulation.
    - Fixed Tensor3 not being initialized in the finit_mdp constructor.
    - FiniteMDP class with basic getters. No tests written.

--- constructs/include/finite_mdp.h
+++ constructs/include/finite_mdp.h
@@ -3,6 +3,7 @@
 
 #include "common/include/error_macros.h"
 #include "math/matrix/include/matrix_checks.h"
+#include "math/tensor/include/tensor3.h"
 #include "third_party/Eigen/Dense"
 
 #include <iostream>
@@ -13,12 +14,28 @@ namespace constructs {
 class FiniteMDP {
 
   public:
-    FiniteMDP(int num_states);
-    FiniteMDP(Eigen::MatrixXd state_transition_matrix);
+    FiniteMDP(int num_states, int num_actions, double gamma);
+    FiniteMDP(mnyrve::math::Tensor3<double> state_transition_tensor,
+              int num_actions,
+              double gamma);
+    FiniteMDP(mnyrve::math::Tensor3<double> state_transition_tensor,
+              int num_actions,
+              Eigen::MatrixXd expected_rewars, 
+              double gamma);
+
+    int GetNumStates();
+    int GetNumActions();
+    double GetGamma();
+    mnyrve::math::Tensor3<double> GetStateTransitionTensor();
+    Eigen::MatrixXd GetExpectedRewards();
+
 
   private:
     int num_states_;
-    Eigen::MatrixXd state_transition_matrix_;
+    int num_actions_;
+    double gamma_;
+    mnyrve::math::Tensor3<double> state_transition_tensor_;
+    Eigen::MatrixXd expected_rewards_;
 
 };
 
--- constructs/src/finite_mdp.cc
+++ constructs/src/finite_mdp.cc
@@ -1,27 +1,75 @@
 #include "constructs/include/finite_mdp.h"
 
 
+using Eigen::MatrixXd;
 using mnyrve::math::IsStochasticMatrix;
 using mnyrve::math::IsSquare;
+using mnyrve::math::Tensor3;
 
 
 namespace mnyrve {
 namespace constructs {
 
-FiniteMDP::FiniteMDP(int num_states):
-  num_states_{num_states} {}
+FiniteMDP::FiniteMDP(int num_states, int num_actions, double gamma):
+  num_states_{num_states}, num_actions_{num_actions}, gamma_{gamma},
+  state_transition_tensor_{num_actions, num_states, num_states}, 
+  expected_rewards_{MatrixXd::Zero(num_states, num_actions)} {}
 
-FiniteMDP::FiniteMDP(
-    Eigen::MatrixXd state_transition_matrix) {
 
-  MN_REQUIRE(IsSquare(state_transition_matrix), 
-     "The state transition matrix must be a square matrix.");
-  MN_REQUIRE(IsStochasticMatrix(state_transition_matrix),
-      "The rows of the state transition matrix must sum to one.");
-  
-  state_transition_matrix_ = state_transition_matrix;
+FiniteMDP::FiniteMDP(Tensor3<double> state_transition_tensor, 
+                     int num_actions, 
+                     double gamma):
+  num_states_{state_transition_tensor.rows()},
+  num_actions_{state_transition_tensor.depth()}, gamma_{gamma}, 
+  state_transition_tensor_{state_transition_tensor}, 
+  expected_rewards_{MatrixXd::Zero(num_states_, num_actions_)} {
+
+    for (int i = 0; i < num_states_; i++) {
+      MN_REQUIRE(IsSquare(state_transition_tensor(i)), 
+            "Each state transition matrix must be square.");
+      MN_REQUIRE(IsStochasticMatrix(state_transition_tensor(i)), 
+          "The rows of each state transition matrix must sum to one.");
+    }
+}
+
+FiniteMDP::FiniteMDP(Tensor3<double> state_transition_tensor, 
+                     int num_actions, 
+                     MatrixXd expected_rewards, 
+                     double gamma):
+  num_states_{state_transition_tensor.rows()},
+  num_actions_{state_transition_tensor.depth()}, gamma_{gamma}, 
+  state_transition_tensor_{state_transition_tensor}, 
+  expected_rewards_{expected_rewards} {
+
+    for (int i = 0; i < num_states_; i++) {
+      MN_REQUIRE(IsSquare(state_transition_tensor(i)), 
+            "Each state transition matrix must be square.");
+      MN_REQUIRE(IsStochasticMatrix(state_transition_tensor(i)), 
+          "The rows of each state transition matrix must sum to one.");
+    }
 
 }
+
+int FiniteMDP::GetNumStates() {
+  return num_states_;
+}
+
+int FiniteMDP::GetNumActions() {
+  return num_actions_;
+}
+
+double FiniteMDP::GetGamma() {
+  return gamma_;
+}
+
+Tensor3<double> FiniteMDP::GetStateTransitionTensor() {
+  return state_transition_tensor_;
+}
+
+MatrixXd FiniteMDP::GetExpectedRewards() {
+  return expected_rewards_;
+}
+
 
 
 } // namespace constructs
