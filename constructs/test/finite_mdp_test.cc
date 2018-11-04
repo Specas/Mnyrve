530372423af8b3c515235dac7ec36de12ab58ba8
shrenikm <shrenik95@gmail.com>
2018-11-04 02:57:57 -0500

    Finite mdp initial formulation.
    - Fixed Tensor3 not being initialized in the finit_mdp constructor.
    - FiniteMDP class with basic getters. No tests written.

---
+++ constructs/test/finite_mdp_test.cc
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "constructs/include/finite_mdp.h"
+
+namespace {
+
+
+
+} // namespace
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+
